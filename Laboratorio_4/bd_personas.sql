1722320e5691161d83bb4d6a3f037ccbde1eb55d
vladip11 <yorp44444@gmail.com>
2019-12-02 23:58:03 -0400

    laboratorio ajax_fecha 02_12_2019

---
+++ Laboratorio_4/bd_personas.sql
@@ -0,0 +1,71 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 03-12-2019 a las 04:56:28
+-- Versión del servidor: 10.4.6-MariaDB
+-- Versión de PHP: 7.3.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `bd_personas`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `persona`
+--
+
+CREATE TABLE `persona` (
+  `id` int(10) NOT NULL,
+  `nombre` varchar(30) COLLATE utf8_bin NOT NULL,
+  `apellido` varchar(30) COLLATE utf8_bin NOT NULL,
+  `correo` varchar(30) COLLATE utf8_bin NOT NULL,
+  `password` varchar(10) COLLATE utf8_bin NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Volcado de datos para la tabla `persona`
+--
+
+INSERT INTO `persona` (`id`, `nombre`, `apellido`, `correo`, `password`) VALUES
+(1, 'juan', 'peres', '123@abc.com', '123'),
+(2, 'jaime', 'duran', '123@abc.com', '789'),
+(3, 'maria', 'jimenez', 'oca@456.com', '456');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `persona`
+--
+ALTER TABLE `persona`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `persona`
+--
+ALTER TABLE `persona`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ Laboratorio_4/pag2.php
@@ -0,0 +1,10 @@
+<div id="contenido2" style="background-color: white ;width: 400px ; height: 200px">
+	<br><br>
+<input type="text" name="num">
+<input type="button" value="insertar" onclick="most()">
+</div>
+
+<h1>
+<div id="contenido3" style="width: 400px ; height: 200px; text-align: center;">
+</div>
+</h1>
